d5537c9ceb28efe25bf886b209059e518a43b6c5
Aladdin Alhazmi <alaadin.on@gmail.com>
2021-08-07 17:44:38 +0300

    registering

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,13 +2,37 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
+    /* ---------------
+
+    -----------------*/
     public function index()
     {
         return view('auth.register');
+    }
+    //
+    public function store(Request $request)
+    {
+        // dd($request->name, $request->username, $request->email, $request->password);
+        $this->validate($request, [
+            'name'=>'required|max:10',
+            'username'=>'required|max:5',
+            'email'=>'required|email|max:100',
+            'password'=>'required|confirmed',
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email'=>$request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aladdin</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}" />
 </head>
-<body>
+<body class="bg-gray-200">
+    <nav class ="p-6 bg-white flex justify-between">
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="p-3">Home</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Dashboard</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Post</a>
+            </li>
+        </ul>
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="p-3">Aladdin</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Login</a>
+            </li>
+            <li>
+                <a href="{{route('register')}}" class="p-3">Register</a>
+            </li>
+        </ul>
+    </nav>
     @yield('content')
 </body>
 </html>
