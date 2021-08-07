597a70121217e5573bcab6885226b60d094ddb75
Aladdin Alhazmi <alaadin.on@gmail.com>
2021-08-07 18:34:44 +0300

    login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,22 +14,29 @@
                 <a href="" class="p-3">Home</a>
             </li>
             <li>
-                <a href="" class="p-3">Dashboard</a>
+                <a href="{{route('dashboard')}}" class="p-3">Dashboard</a>
             </li>
             <li>
                 <a href="" class="p-3">Post</a>
             </li>
         </ul>
         <ul class="flex items-center">
-            <li>
-                <a href="" class="p-3">Aladdin</a>
-            </li>
-            <li>
-                <a href="" class="p-3">Login</a>
-            </li>
-            <li>
-                <a href="{{route('register')}}" class="p-3">Register</a>
-            </li>
+            @auth
+                <li>
+                    <a href="" class="p-3">Aladdin</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">Logout</a>
+                </li>
+            @endauth
+            @guest
+                <li>
+                    <a href="{{route('login')}}" class="p-3">Login</a>
+                </li>
+                <li>
+                    <a href="{{route('register')}}" class="p-3">Register</a>
+                </li>
+            @endguest
         </ul>
     </nav>
     @yield('content')
